f7ef8a7affb9435f92c3b30e3c6985c45447bb4a
Jeongsoo Park <toracle@gmail.com>
2020-04-19 00:53:31 +0900

    generate card by code, refactor codes

--- src/moving_motivator/script.js
+++ src/moving_motivator/script.js
@@ -1,28 +1,46 @@
-function mapKey(keyCode) {
-  const mapping = {
-    37: "left",
-    39: "right",
-    38: "up",
-    40: "down",
-  };
+const keyboardMapping = {
+  37: "left",
+  39: "right",
+  38: "up",
+  40: "down",
+};
 
-  return mapping[keyCode];
+const keyboardCtrlMapping = {
+  37: "shift-left",
+  39: "shift-right",
 }
 
-function focusNextCard(direction = 1) {
+function mapKey(event) {
+  const ctrlPressed = event.ctrlKey;
+
+  if (ctrlPressed) {
+    return keyboardCtrlMapping[event.keyCode];
+  }
+  return keyboardMapping[event.keyCode];
+}
+
+function getCards() {
   const cards = document.querySelectorAll(".card");
+  return cards;
+}
+
+function getActiveCard() {
   const activeElement = document.activeElement;
 
   if (activeElement.className != "card") {
-    cards[0].focus();
-    return;
+    return null;
   }
-
-  const nextCard = getNextCard(cards, activeElement, direction);
-  nextCard.focus();
+  return activeElement;
 }
 
-function getNextCard(cards, activeCard, direction = 1) {
+function getNextCard(direction = 1) {
+  const cards = getCards();
+  const activeCard = getActiveCard();
+
+  if (activeCard == null) {
+    return cards[0];
+  }
+
   var index = null;
 
   for (var i = 0; i < cards.length; i++) {
@@ -33,18 +51,119 @@ function getNextCard(cards, activeCard, direction = 1) {
   }
 
   if (index == cards.length - 1 && direction == 1) {
-    return cards[0];
+    return null;
   }
   if (index == 0 && direction == -1) {
-    return cards[cards.length-1];
+    return null;
   }
   return cards[index + direction];
+}
+
+function focusNextCard(direction = 1) {
+  const nextCard = getNextCard(direction);
+  if (!nextCard) {
+    return;
+  }
+  nextCard.focus();
 }
 
 function actionToDirection(action) {
   const actionToDirectionMap = {
     "left": -1,
     "right": 1,
+    "shift-left": -1,
+    "shift-right": 1,
   }
   return actionToDirectionMap[action];
+}
+
+function shiftCard(direction) {
+  console.log("shift to " + direction);
+  const activeCard = getActiveCard();
+  const nextCard = getNextCard(direction);
+}
+
+function keyboardHandler(event) {
+  var action = mapKey(event);
+  console.log(action);
+
+  if (action === undefined) {
+    return;
+  }
+
+  const direction = actionToDirection(action);
+  if (!direction) {
+    return;
+  }
+
+  const actionToFunction = {
+    "right": focusNextCard,
+    "left": focusNextCard,
+    "shift-right": shiftCard,
+    "shift-left": shiftCard,
+  }
+
+  const fn = actionToFunction[action];
+  if (!fn) {
+    return;
+  }
+  fn(direction);
+}
+
+function initCard() {
+  const cardModels = [
+    {
+      'id': 'curiosity',
+      'name': '호기심',
+    },
+    {
+      'id': 'honor',
+      'name': '명예',
+    },
+    {
+      'id': 'acceptance',
+      'name': '수용',
+    },
+    {
+      'id': 'mastery',
+      'name': '전문성',
+    },
+    {
+      'id': 'power',
+      'name': '파워',
+    },
+    {
+      'id': 'freedom',
+      'name': '자유',
+    },
+    {
+      'id': 'relatedness',
+      'name': '연결됨',
+    },
+    {
+      'id': 'order',
+      'name': '질서',
+    },
+    {
+      'id': 'goal',
+      'name': '목표',
+    },
+    {
+      'id': 'status',
+      'name': '지위',
+    },
+  ];
+
+  const cardHolder = document.getElementById("card-holder");
+  cardModels.forEach(function(item) {
+    // cardHolder.
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+    card.setAttribute("draggable", true);
+    card.setAttribute("tabindex", 0);
+    card.setAttribute("id", 'card-' + item['id']);
+    card.textContent = item['name'];
+
+    cardHolder.appendChild(card);
+  });
 }
