09df889400305a6b0d89ffd031f62ac68924283a
Jeongsoo Park <toracle@gmail.com>
2020-04-19 19:58:02 +0900

    implement submit function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const keyboardMapping = {
   39: "right",
   38: "up",
   40: "down",
+  13: "enter",
 };
 
 const keyboardCtrlMapping = {
@@ -41,7 +42,6 @@ function getActiveCard() {
 function getNextCard(direction = 1, card) {
   const cards = getCards();
   const activeCard = card || getActiveCard();
-  console.log(activeCard);
 
   if (activeCard == null) {
     return cards[0];
@@ -118,6 +118,7 @@ function setLevel(card, level) {
                         "level-low",
                         "level-verylow");
   card.classList.add("level-" + level);
+  card.setAttribute("level", level);
 }
 
 function getLevel(card) {
@@ -190,6 +191,7 @@ function keyboardHandler(event) {
     "shift-left": shiftCard,
     "up": increaseLevel,
     "down": decreaseLevel,
+    "enter": submit,
   }
 
   const fn = actionToFunction[action];
@@ -201,57 +203,99 @@ function keyboardHandler(event) {
   fn(direction);
 }
 
+function gather() {
+  const cards = getCards();
+
+  const results = new Array();
+  cards.forEach(function(item, idx) {
+    item.setAttribute('final-order', idx + 1);
+    results.push({
+      'id': item.getAttribute('uid'),
+      'initial-order': parseInt(item.getAttribute('initial-order')),
+      'final-order': parseInt(item.getAttribute('final-order')),
+      'level': item.getAttribute('level'),
+    });
+  });
+  return results;
+}
+
+function submit() {
+  const result = gather();
+  console.table(result);
+  setFlash('결과를 제출했습니다.');
+}
+
+function setFlash(text, timeout = 3000) {
+  const flashElement = document.getElementById('flash');
+  flashElement.textContent = text;
+
+  setTimeout(function() {
+    const flashElement = document.getElementById('flash');
+    flashElement.textContent = '';
+  }, timeout);
+}
+
 function initCard() {
   const cardModels = [
     {
       'id': 'curiosity',
       'name': '호기심',
       'content': '나는 탐구하거나 생각할 것이 많다',
+      'initial-order': 1,
     },
     {
       'id': 'honor',
       'name': '명예',
       'content': '나는 내가 일하는 방식에, 내 개인적인 가치관이 반영되는 것이 자랑스럽게 느껴진다',
+      'initial-order': 2,
     },
     {
       'id': 'acceptance',
       'name': '수용',
       'content': '내 주위에 있는 사람들은 내가 하는 것과 내가 어떤 사람인지를 인정한다',
+      'initial-order': 3,
     },
     {
       'id': 'mastery',
       'name': '전문성',
       'content': '내 업무가 내 역량에 도전이 되면서도, 여전히 내 능력 범위 안에 있다',
+      'initial-order': 4,
     },
     {
       'id': 'power',
       'name': '파워',
       'content': '내게는 내 주위에서 일어나는 일들에 대해 영향력을 미칠 수 있는 충분한 공간이 있다',
+      'initial-order': 5,
     },
     {
       'id': 'freedom',
       'name': '자유',
       'content': '나는 내 일과 책임에서 다른 사람과 독립되어 있다',
+      'initial-order': 6,
     },
     {
       'id': 'relatedness',
       'name': '연결됨',
       'content': '나는 내 업무에서 사람들과 좋은 사회적 관계를 이루고 있다',
+      'initial-order': 7,
     },
     {
       'id': 'order',
       'name': '질서',
       'content': '안정된 환경을 위해서 충분한 규칙과 정책이 존재한다',
+      'initial-order': 8,
     },
     {
       'id': 'goal',
       'name': '목표',
       'content': '내 삶의 목적이 내가 하는 일에 잘 반영되어 있다',
+      'initial-order': 9,
     },
     {
       'id': 'status',
       'name': '지위',
       'content': '내 포지션은 괜찮다. 그리고 함께 일하는 사람들도 그걸 인정한다',
+      'initial-order': 10,
     },
   ];
 
@@ -263,8 +307,12 @@ function initCard() {
     card.setAttribute("draggable", true);
     card.setAttribute("tabindex", 0);
     card.setAttribute("id", 'card-' + item['id']);
+    card.setAttribute("uid", item['id']);
     card.setAttribute("name", item['name']);
     card.setAttribute("content", item['content']);
+    card.setAttribute("level", "normal");
+    card.setAttribute("initial-order", item['initial-order']);
+    card.setAttribute("final-order", item['initial-order']);
     card.textContent = item['name'];
 
     cardHolder.appendChild(card);
