9e73b2fd8ccb6568d0c4edb0b676e57c63aa3e6e
Jeongsoo Park <toracle@gmail.com>
2020-04-19 19:11:18 +0900

    add a card description area

--- script.js
+++ script.js
@@ -65,12 +65,18 @@ function getNextCard(direction = 1, card) {
   return cards[index + direction];
 }
 
+function setContent(text) {
+  const descBox = document.getElementById('card-description');
+  descBox.textContent = text;
+}
+
 function focusNextCard(direction = 1) {
   const nextCard = getNextCard(direction);
   if (!nextCard) {
     return;
   }
   nextCard.focus();
+  setContent(nextCard.getAttribute('name') + ': ' + nextCard.getAttribute('content'));
 }
 
 function actionToDirection(action) {
@@ -200,42 +206,52 @@ function initCard() {
     {
       'id': 'curiosity',
       'name': '호기심',
+      'content': '나는 탐구하거나 생각할 것이 많다',
     },
     {
       'id': 'honor',
       'name': '명예',
+      'content': '나는 내가 일하는 방식에, 내 개인적인 가치관이 반영되는 것이 자랑스럽게 느껴진다',
     },
     {
       'id': 'acceptance',
       'name': '수용',
+      'content': '내 주위에 있는 사람들은 내가 하는 것과 내가 어떤 사람인지를 인정한다',
     },
     {
       'id': 'mastery',
       'name': '전문성',
+      'content': '내 업무가 내 역량에 도전이 되면서도, 여전히 내 능력 범위 안에 있다',
     },
     {
       'id': 'power',
       'name': '파워',
+      'content': '내게는 내 주위에서 일어나는 일들에 대해 영향력을 미칠 수 있는 충분한 공간이 있다',
     },
     {
       'id': 'freedom',
       'name': '자유',
+      'content': '나는 내 일과 책임에서 다른 사람과 독립되어 있다',
     },
     {
       'id': 'relatedness',
       'name': '연결됨',
+      'content': '나는 내 업무에서 사람들과 좋은 사회적 관계를 이루고 있다',
     },
     {
       'id': 'order',
       'name': '질서',
+      'content': '안정된 환경을 위해서 충분한 규칙과 정책이 존재한다',
     },
     {
       'id': 'goal',
       'name': '목표',
+      'content': '내 삶의 목적이 내가 하는 일에 잘 반영되어 있다',
     },
     {
       'id': 'status',
       'name': '지위',
+      'content': '내 포지션은 괜찮다. 그리고 함께 일하는 사람들도 그걸 인정한다',
     },
   ];
 
@@ -247,6 +263,8 @@ function initCard() {
     card.setAttribute("draggable", true);
     card.setAttribute("tabindex", 0);
     card.setAttribute("id", 'card-' + item['id']);
+    card.setAttribute("name", item['name']);
+    card.setAttribute("content", item['content']);
     card.textContent = item['name'];
 
     cardHolder.appendChild(card);
