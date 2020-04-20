024b477485cae035f157fd402979b42f8250e88b
Jeongsoo Park <toracle@gmail.com>
2020-04-21 01:17:28 +0900

    separate card model to cards.js

---
+++ cards.js
@@ -0,0 +1,62 @@
+const cardModels = [
+  {
+    'id': 'curiosity',
+    'name': '호기심',
+    'content': '나는 탐구하거나 생각할 것이 많다',
+    'initial-order': 1,
+  },
+  {
+    'id': 'honor',
+    'name': '명예',
+    'content': '나는 내가 일하는 방식에, 내 개인적인 가치관이 반영되는 것이 자랑스럽게 느껴진다',
+    'initial-order': 2,
+  },
+  {
+    'id': 'acceptance',
+    'name': '수용',
+    'content': '내 주위에 있는 사람들은 내가 하는 것과 내가 어떤 사람인지를 인정한다',
+    'initial-order': 3,
+  },
+  {
+    'id': 'mastery',
+    'name': '전문성',
+    'content': '내 업무가 내 역량에 도전이 되면서도, 여전히 내 능력 범위 안에 있다',
+    'initial-order': 4,
+  },
+  {
+    'id': 'power',
+    'name': '파워',
+    'content': '내게는 내 주위에서 일어나는 일들에 대해 영향력을 미칠 수 있는 충분한 공간이 있다',
+    'initial-order': 5,
+  },
+  {
+    'id': 'freedom',
+    'name': '자유',
+    'content': '나는 내 일과 책임에서 다른 사람과 독립되어 있다',
+    'initial-order': 6,
+  },
+  {
+    'id': 'relatedness',
+    'name': '연결됨',
+    'content': '나는 내 업무에서 사람들과 좋은 사회적 관계를 이루고 있다',
+    'initial-order': 7,
+  },
+  {
+    'id': 'order',
+    'name': '질서',
+    'content': '안정된 환경을 위해서 충분한 규칙과 정책이 존재한다',
+    'initial-order': 8,
+  },
+  {
+    'id': 'goal',
+    'name': '목표',
+    'content': '내 삶의 목적이 내가 하는 일에 잘 반영되어 있다',
+    'initial-order': 9,
+  },
+  {
+    'id': 'status',
+    'name': '지위',
+    'content': '내 포지션은 괜찮다. 그리고 함께 일하는 사람들도 그걸 인정한다',
+    'initial-order': 10,
+  },
+];
